7fce3a25fbd6ba4919fc3312776fcf090eb74a05
puhrome <phirom.yim@fullsail.edu>
2014-05-15 14:49:21 -0400

    Alert
    Setting up Alerts in WebStorm and JS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Alert! Created by Phirom Yim on 5/15/14.
+ */
+/*var yearBorn = 1955; //assign variable
+
+console.log(yearBorn);//print to console, will display whatever you put in parenthesis
+
+console.log("Hello World");//can also print string variable */
+
+alert("Hello World");//will print like console.log but will print out a popup
+//serves a purpose as a error, still considered a little old fashion and phased professionally
